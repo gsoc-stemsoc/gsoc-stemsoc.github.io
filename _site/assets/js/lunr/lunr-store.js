efc8dc1c652cb1b1d361c7fc90e51354166428d3
birdday <22bday@gmail.com>
2020-07-01 15:29:24 -0400

    Added all necessary resources for website with jekyll/

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "WeHaul Healthcare wins 2nd place at IBM BlueHack!",
+        "excerpt":"November 1st, 2019 During the 2019 IBM BlueHack hackathon, our team of ChemE PhD students from the Wilmer Lab (Myself and Kutay), Little Lab (Andrea and Ashlee), and Li Lab (Aigerim) beat out 19 other teams for a second place finish with our idea, WeHaul Healthcare. The hackathon was a...","categories": ["posts"],
+        "tags": [],
+        "url": "http://localhost:4000/posts/ibm-bluehack/",
+        "teaser":"http://localhost:4000/assets/img/posts/WeHaul_Team_teaser.png"},{
+        "title": "Winner of the PQI travel award",
+        "excerpt":"April 17th, 2020   Held in April, amidst the COVID-19 pandemic, I participated in and won the (online) 2020 poster competition, recieving the 2020 Pittsburgh Quantum Institute travel award. I really enjoyed seeing everyone’s unique and impressive projects. Check out my winning poster below!     ","categories": ["posts"],
+        "tags": [],
+        "url": "http://localhost:4000/posts/brian-pqi/",
+        "teaser":"http://localhost:4000/assets/img/posts/pqi_poster_2020_teaser.jpg"}]
